e31286eb79b9ab23a19792c88a96d714da19c315
JD Singh <info@fermatsolutions.dev>
2024-10-21 20:51:58 -0500

    initial commit

---
+++ src/test/kotlin/dev/fermatsolutions/ApplicationTest.kt
@@ -0,0 +1,31 @@
+package dev.fermatsolutions
+
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ApplicationTest {
+
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            module()
+        }
+        val response = client.get("/")
+        assertEquals(HttpStatusCode.OK, response.status)
+        assertEquals("Hello World!", response.bodyAsText())
+    }
+
+    @Test
+    fun testNewEndpoint() = testApplication {
+        application {
+            module()
+        }
+        val response = client.get("/test1")
+        assertEquals(HttpStatusCode.OK, response.status)
+        assertEquals("html", response.contentType()?.contentSubtype)
+    }
+}
