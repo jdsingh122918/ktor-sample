1c3158a744fa0a20eb2d146260242d5f1c806562
JD Singh <info@fermatsolutions.dev>
2024-10-22 13:50:45 -0500

    Added task ui

--- src/test/kotlin/dev/fermatsolutions/ApplicationTest.kt
+++ src/test/kotlin/dev/fermatsolutions/ApplicationTest.kt
@@ -5,6 +5,7 @@ import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
 import kotlin.test.Test
+import kotlin.test.assertContains
 import kotlin.test.assertEquals
 
 class ApplicationTest {
@@ -27,5 +28,49 @@ class ApplicationTest {
         val response = client.get("/test1")
         assertEquals(HttpStatusCode.OK, response.status)
         assertEquals("html", response.contentType()?.contentSubtype)
+    }
+
+    @Test
+    fun tasksCanBeFoundByPriority() = testApplication {
+        application {
+            module()
+        }
+        val response = client.get("/tasks/byPriority/Medium")
+        assertEquals(HttpStatusCode.OK, response.status)
+    }
+
+    @Test
+    fun unusedPriorityReturns404() = testApplication {
+        application {
+            module()
+        }
+        val response = client.get("/tasks/byPriority/High")
+        assertEquals(HttpStatusCode.NotFound, response.status)
+    }
+
+    @Test
+    fun newTaskCanBeAdded() = testApplication {
+        application {
+            module()
+        }
+
+        val response = client.post("/tasks") {
+            header(HttpHeaders.ContentType, ContentType.Application.FormUrlEncoded.toString())
+            setBody(
+                listOf(
+                    "name" to "Test Task",
+                    "description" to "This is a test task",
+                    "priority" to "Low"
+                ).formUrlEncode()
+            )
+        }
+
+        assertEquals(HttpStatusCode.NoContent, response.status)
+
+        val tasksListResponse = client.get("/tasks")
+        assertEquals(HttpStatusCode.OK, tasksListResponse.status)
+        val tasksListBody = tasksListResponse.bodyAsText()
+
+        assertContains(tasksListBody, "Test Task")
     }
 }
